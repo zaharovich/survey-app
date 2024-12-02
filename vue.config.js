78b9349144a8ff229e5de7ec2f0783b352b89480
zahar <spacezahar@gmail.com>
2024-12-02 21:48:52 +0200

    Deploy updated build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/",
+  publicPath: "/survey-app.github.io/",
   transpileDependencies: true,
 });
